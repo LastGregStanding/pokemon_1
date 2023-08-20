54a5addd00a712595266c044128892ae92c921d5
LastGregStanding <125837709+LastGregStanding@users.noreply.github.com>
2023-08-19 18:59:43 -0700

    added some js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const move = document.querySelectorAll(".move");
+
+// Hover each move
+move.forEach((move) => {
+  move.addEventListener("mouseover", function () {
+    move.classList.add("hover");
+  });
+  move.addEventListener("mouseout", function () {
+    move.classList.remove("hover");
+  });
+  move.addEventListener("click", function () {
+    console.log(`you chose ${move.textContent} `);
+  });
+});
